8e6ba15675f923f7b2a43df34fc6614b6fc7e8ce
PyroSage <studybhaskar654@gmail.com>
2023-03-28 11:35:10 +0530

    commenting

--- checkpoint3/lidar_sub_node.py
+++ checkpoint3/lidar_sub_node.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
+
+# Import necessary ROS packages and message types
 import rospy as rp
 from std_msgs.msg import String 
 
+# Define callback function to be executed when a message is received on the "/lidar_info/chatter" topic
 def callback(msg):
     rp.loginfo("I heard: %s", msg.data)
 
+# Define another callback function to be executed when a message is received on the "/lidar_info/three" topic
 def other(msg):
     rp.loginfo("I heard: %s", msg.data)
 
+# Define listener function that creates subscribers to the two topics and initializes the node
 def listener():
+    # Create a subscriber to the "/lidar_info/chatter" topic with the callback function "callback"
     rp.Subscriber("/lidar_info/chatter", String, callback)
+    # Create another subscriber to the "/lidar_info/three" topic with the callback function "other"
     rp.Subscriber("/lidar_info/three", String, callback=other)
+    # Initialize the node with the name "listener" and anonymous flag set to True
     rp.init_node('listener', anonymous=True)
-    
+
+# Main function that calls the listener function and enters a loop to keep the node alive
 if __name__ == '__main__':
+    # Call the listener function to create subscribers and initialize the node
     listener()
+    # Enter a loop to keep the node alive
     rp.spin()
--- checkpoint3/lidar_sub_pub_node.py
+++ checkpoint3/lidar_sub_pub_node.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import rospy as rp
 from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud2
@@ -7,27 +8,29 @@ import numpy as np
 import random
 from datetime import datetime
 
-
+# Define a class for the lidar subscriber node
 class lidar_sub_node:
 
     def __init__(self):
         self.my_data = ''
         rp.init_node('listener', anonymous=True)
+        # Subscribe to the PointCloud2 topic "/fsds/lidar/Lidar1"
         rp.Subscriber("/fsds/lidar/Lidar1", PointCloud2, self.callback_func)
         self.rate = rp.Rate(1)
+        # Define two publishers for the String topics "/lidar_info/chatter" and "/lidar_info/three"
         self.pub = rp.Publisher("/lidar_info/chatter", String, queue_size= 10)
         self.pub2 = rp.Publisher("/lidar_info/three", String, queue_size=10)
         self.xyz = []
         
         self.time = str(datetime.now())
         
-        
+    # Define a callback function to read the XYZ coordinates of the lidar points
     def callback_func(self, msg):
         points = np.around(list(pc2.read_points(msg, skip_nans=True)), decimals=2)
         self.xyz = points[:,:]
         self.my_data = "XYZ coordinates: \n{}".format(self.xyz)
-        
     
+    # Define a talker function to publish random yaw, velocity and time information to the two topics
     def talker(self):
         while not rp.is_shutdown():
             self.yaw = random.randrange(1000)
